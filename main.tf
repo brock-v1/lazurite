1dce8c21bbead26944df14fb8905c24b1b32ca04
Brock <Brock@Brocks-MacBook-Pro.local>
2024-06-21 22:23:27 -0400

    added VNET with default subnet

--- main.tf
+++ main.tf
@@ -15,3 +15,15 @@ resource "azurerm_resource_group" "RG" {
   name     = "RG"
   location = "East US"
 }
+
+resource "azurerm_virtual_network" "VNET" {
+  name                = "VNET"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.0.0/24"
+  }
+}
