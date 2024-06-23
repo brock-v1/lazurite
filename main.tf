1c2196da60d6aacc48cb5b7e43d0a46c252e552f
Brock <Brock@Brocks-MacBook-Pro.local>
2024-06-23 05:09:19 -0400

    Added 2 VMs with Web Server on 80 + opened NSG up to allow HTTP traffic. Added outputs file to display the VMs' randomly generated PWs and IPs. Added variables file that currently only contains a variable for the region of the resource group and consequently all region-bound resources.

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+#Define Providers
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
     }
   }
 }
@@ -13,7 +18,7 @@ provider "azurerm" {
 #Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg"
-  location = "East US"
+  location = var.resource_group_location
 }
 
 #Create 2 VNETs with Peering
@@ -69,6 +74,19 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
+resource "azurerm_network_security_rule" "allowany80" {
+  name                        = "allowany80"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
 resource "azurerm_subnet_network_security_group_association" "nsgtovnet1-subnet1" {
   subnet_id                 = azurerm_subnet.vnet1-subnet1.id
   network_security_group_id = azurerm_network_security_group.nsg.id
@@ -114,3 +132,147 @@ resource "azurerm_network_watcher_flow_log" "nsgflowlog" {
     interval_in_minutes   = 10
   }
 }
+
+#Create VM1, in VNET1. Enable IIS Web Server.
+resource "random_password" "vm1-pw" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+resource "azurerm_public_ip" "vm1-public-ip" {
+  name                = "vm1-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+resource "azurerm_network_interface" "vm1-nic" {
+  name                = "vm1-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "vm1-nic-ipconfig"
+    subnet_id                     = azurerm_subnet.vnet1-subnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.1.1.5"
+    public_ip_address_id          = azurerm_public_ip.vm1-public-ip.id
+  }
+}
+resource "azurerm_windows_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "lazurite"
+  admin_password      = random_password.vm1-pw.result
+  network_interface_ids = [
+    azurerm_network_interface.vm1-nic.id,
+  ]
+  os_disk {
+    name                 = "vm1-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+}
+resource "azurerm_virtual_machine_extension" "vm1-webserverinstall" {
+  name                       = "vm1-webserverinstall"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm1.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+  SETTINGS
+}
+
+#Create VM2, in VNET2. Enable IIS Web Server.
+resource "random_password" "vm2-pw" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+resource "azurerm_public_ip" "vm2-public-ip" {
+  name                = "vm2-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+resource "azurerm_network_interface" "vm2-nic" {
+  name                = "vm2-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "vm2-nic-ipconfig"
+    subnet_id                     = azurerm_subnet.vnet2-subnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.2.1.5"
+    public_ip_address_id          = azurerm_public_ip.vm2-public-ip.id
+  }
+}
+resource "azurerm_windows_virtual_machine" "vm2" {
+  name                = "vm2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "lazurite"
+  admin_password      = random_password.vm2-pw.result
+  network_interface_ids = [
+    azurerm_network_interface.vm2-nic.id,
+  ]
+  os_disk {
+    name                 = "vm2-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+}
+resource "azurerm_virtual_machine_extension" "vm2-webserverinstall" {
+  name                       = "vm2-webserverinstall"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm2.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+  SETTINGS
+}
+
+##########################################################################################################################
+
+# TO DO LIST:
+
+# Need to add the below for Hello World page
+# Remove-Item C:\inetpub\wwwroot\iisstart.htm"
+# Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello World from "+$env:computername)"
+
+# Need to write PowerShell script or something that automatically adds an NSG rule for my public IP allowing 80/3389
+
+# Need to randomize resource names
+
+# Need to create variables.tf for things like region, etc.
+
+##########################################################################################################################
---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vm1_ip" {
+  value = azurerm_windows_virtual_machine.vm1.public_ip_address
+}
+output "vm1_pw" {
+    sensitive = true
+  value     = azurerm_windows_virtual_machine.vm1.admin_password
+}
+output "vm2_ip" {
+  value = azurerm_windows_virtual_machine.vm2.public_ip_address
+}
+output "vm2_pw" {
+    sensitive = true
+  value     = azurerm_windows_virtual_machine.vm2.admin_password
+}
