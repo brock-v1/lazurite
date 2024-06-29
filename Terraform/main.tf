52bf50ebad2c09e5a0d98aa983dee4090b9e7ed8
Brock <Brock@Brocks-MacBook-Pro.local>
2024-06-29 07:00:16 -0400

    Added Bastion

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -325,6 +325,37 @@ resource "azurerm_lb_backend_address_pool_address" "vm2" {
 
 ##########################################################################################################################
 
+# Bastion
+
+resource "azurerm_subnet" "bastion-subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.1.99.0/27"]
+}
+
+resource "azurerm_public_ip" "bastion-ip" {
+  name                = "bastion-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion-host" {
+  name                = "bastionhost"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "bastion-ipconfig"
+    subnet_id            = azurerm_subnet.bastion-subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion-ip.id
+  }
+}
+
+##########################################################################################################################
+
 # TO DO LIST:
 
 # Add Bastion
